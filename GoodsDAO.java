c17d37aa207db960fddd16d3ada1a30d164a34bb
mochoi42 <principle950@naver.com>
2024-07-17 16:51:17 +0900

    project 생성

---
+++ GoodsDAO.java
@@ -0,0 +1,10 @@
+package myProject.testc;
+
+public class GoodsDAO {
+    public void insert(){
+        System.out.println("insert goods OK");
+    }
+    public void update(){
+        System.out.println("update goods OK");
+    }
+}
---
+++ Hello.java
@@ -0,0 +1,9 @@
+package myProject.testc;
+
+public class Hello {
+    public static void main(String []args){
+        GoodsDAO dao = new GoodsDAO();
+        dao.insert();
+        dao.update();
+    }
+}
